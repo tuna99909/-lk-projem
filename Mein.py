3052388baa0d421fa92c5ddf37a48c5a601ff8d3
tuna99909 <tuna.turan.907@gmail.com>
2025-05-22 19:51:46 +0300

    Update Mein.py

--- Mein.py
+++ Mein.py
@@ -1,6 +1,10 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
+            "ROFL": "Bir şakaya karşılık cevap",
+            "SHEESH": "Onaylamamak",
+            "CREEPY": "Korkunç",
+            "AGGRO": "Agresifleşmek/Sinirlenmek",
             }
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
 if word in meme_dict.keys():
